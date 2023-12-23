cf8b0e19619e6e471f330afaa2d16d72e505ce9e
Sergio Osadchuk <116348529+Sergii-Osedchuk@users.noreply.github.com>
2023-12-24 00:23:25 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Player from './components/Player';
+import GameBoard from './components/GameBoard';
+function App() {
+  
+
+  return (
+    <main>
+      <div id='game-container'>
+        <ol id='players'>
+          <Player initialName='Player1' symbol='X' />
+          <Player initialName='Player2' symbol='O' />
+        </ol>
+        <GameBoard/>
+      </div>
+      LOG
+    </main>
+  )
+}
+
+export default App
